2f9eaa46f2d1e26c855358cc4092b0a93e1968c0
Khizer Hussain <157730914+flutions-khizer@users.noreply.github.com>
2024-05-20 21:37:26 +0530

    next update

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
+  reactStrictMode: true,
   output: 'export',
   images: {
     unoptimized: true
   },
-  distDir: 'out', 
+  distDir: './out', 
 }
 
 module.exports = nextConfig
